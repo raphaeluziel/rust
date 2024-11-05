e9e0c034b60c90e2845ba6659b47dc0f172b337d
raphaeluziel <raphaeluziel@hotmail.com>
2024-11-05 08:12:32 -0500

    nothing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -26,5 +26,18 @@ fn main() {
     println!("Guess: {guess}");
     */
 
+    /*
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is {y}");
+    */
+
+    /*
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+    println!("first = {}, second = {}, third = {}", five_hundred, six_point_four, one);
+    */
     
 }
