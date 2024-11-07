1d53c39bd5b67c2c66bbd2efeeb60980eb7f8fb4
raphaeluziel <raphaeluziel@hotmail.com>
2024-11-07 17:27:08 -0500

    branches started

---
+++ branches/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    /*
+    let number = 1;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number aint divisible by 2, 3, or 4");
+    }
+    */
+
+    let condition = false;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is {number}");
+
+}
--- functions/src/main.rs
+++ functions/src/main.rs
@@ -11,6 +11,9 @@ fn main() {
 
     let x = five();
     println!("The value of x is {x}");
+
+    let bb = plus_one(67);
+    println!("The value of BB is {bb}");
 }
 
 fn another_function(x: i32) {
@@ -23,4 +26,8 @@ fn print_labeled_measurements(value: i32, unit_label: char) {
 
 fn five() -> i32 {
     5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
