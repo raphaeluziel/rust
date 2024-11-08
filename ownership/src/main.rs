a15a7003bd32bb6180dbe1e475e99ad5be4e2a68
raphaeluziel <raphaeluziel@hotmail.com>
2024-11-08 15:26:20 -0500

    owner

---
+++ ownership/src/main.rs
@@ -0,0 +1,16 @@
+fn read(y: bool) {
+    if y {
+        println!("y is true");
+    } else {
+        println!("y aint true");
+    }
+}
+
+
+fn main() {
+    read(x);
+    let x = false;
+    
+}
+
+
