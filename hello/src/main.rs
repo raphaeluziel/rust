c5b43bdc80a7d0ee4ec53c8a2f9c0f817262a030
raphaeluziel <raphaeluziel@hotmail.com>
2024-11-13 18:00:32 -0500

    just getting started

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let p = gcd(1786, 0);
+    println!("{p}");
+}
+
+fn gcd(mut n:u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m!= 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
