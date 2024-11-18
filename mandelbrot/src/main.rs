86b92fe844021b08415b3c7292e191ec33128727
raphaeluziel <raphaeluziel@hotmail.com>
2024-11-18 17:14:39 -0500

    still mandy

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use num::Complex;
 
 /// First try, and it doesn't seem to work
@@ -9,6 +10,23 @@ fn complex_square_add_loop(c: Complex<f64>) {
     }
 }
 */
+
+/// Parse the string 's' as a coordinate pair, like `"400x600"` or `"1.0, 0.5"`.
+/// 
+/// Specifically, 's' should have the form <left><sep><right>, where <sep> is the character given by the 'separator' argument, and <left> and <right> are both strings that can be parsed by `T::from_str`.  'separator' must be an ASCII character.
+/// 
+/// If 's' has the proper form, return `Some(x, y)`.  If it doesn't parse correctly, return `None`.
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
 
 /// Try to determine if 'c' is in the Mandelbrot set, using at most 'limit' iterations to decide.
 /// 
@@ -28,4 +46,15 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
 fn main() {
     //let z: Complex<f64> = complex_square_add_loop(Complex {re: 0.06, im: 2.9});
     //println!("{} + {}", z.re, z.im);
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",        ','), None);
+    assert_eq!(parse_pair::<i32>("10,",     ','), None);
+    assert_eq!(parse_pair::<i32>(",10",     ','), None);
+    assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<i32>("0.5x",    'x'), None);
+    assert_eq!(parse_pair::<i32>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
