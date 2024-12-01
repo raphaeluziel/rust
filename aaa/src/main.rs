042938309fca0193b1b93430c248f74110fbfca9
raphaeluziel <raphaeluziel@hotmail.com>
2024-12-01 17:00:05 -0500

    playground

---
+++ aaa/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    struct Person { name: String, birth: i32, }
+
+    let mut composers = Vec::new();
+
+    composers.push(Person { name: "Palestrina".to_string(), birth: 1525 });
+    composers.push(Person { name: "Dowland".to_string(), birth: 1563 });
+    composers.push(Person { name: "Lully".to_string(), birth: 1632 });
+
+    for composer in &composers {
+        println!("{}, born {}", composer.name, composer.birth);
+    }
+}
