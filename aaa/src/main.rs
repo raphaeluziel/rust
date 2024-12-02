812036647fe971c17e811e79c06ff6b8c8765744
raphaeluziel <raphaeluziel@hotmail.com>
2024-12-02 15:34:29 -0500

    progress

--- aaa/src/main.rs
+++ aaa/src/main.rs
@@ -7,7 +7,17 @@ fn main() {
     composers.push(Person { name: "Dowland".to_string(), birth: 1563 });
     composers.push(Person { name: "Lully".to_string(), birth: 1632 });
 
+    /*
     for composer in &composers {
         println!("{}, born {}", composer.name, composer.birth);
     }
+    */
+
+    /*
+    for i in 0..3 {
+        println!("{} born in {}", composers[i].name, composers[i].birth);
+    }
+    */
+
+    println!("{}", composers[0].name);   
 }
