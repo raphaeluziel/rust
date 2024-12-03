ec61446da68fe2e856586d043614ede3609b4760
raphaeluziel <raphaeluziel@gmail.com>
2024-12-03 07:44:57 -0500

    minor change

--- aaa/src/main.rs
+++ aaa/src/main.rs
@@ -7,11 +7,9 @@ fn main() {
     composers.push(Person { name: "Dowland".to_string(), birth: 1563 });
     composers.push(Person { name: "Lully".to_string(), birth: 1632 });
 
-    /*
     for composer in &composers {
         println!("{}, born {}", composer.name, composer.birth);
     }
-    */
 
     /*
     for i in 0..3 {
