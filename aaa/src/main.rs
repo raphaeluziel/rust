a97871ab1686591a0733a8bb16ff02ea5843ef31
raphaeluziel <raphaeluziel@hotmail.com>
2024-12-04 11:32:53 -0500

    tired

--- aaa/src/main.rs
+++ aaa/src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     struct Person { name: String, birth: i32, }
 
@@ -7,11 +8,9 @@ fn main() {
     composers.push(Person { name: "Dowland".to_string(), birth: 1563 });
     composers.push(Person { name: "Lully".to_string(), birth: 1632 });
 
-    /*
     for composer in &composers {
         println!("{}, born {}", composer.name, composer.birth);
     }
-    */
 
     /*
     for i in 0..3 {
@@ -19,5 +18,13 @@ fn main() {
     }
     */
 
-    println!("{}", composers[0].name);   
+    println!("{}", composers[0].name);
+
+
+    // Page 106
+    let x = 14;
+    let r = &x;
+
+    println!("{:p}", r);
+
 }
