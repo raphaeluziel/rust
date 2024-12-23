6354f95b37f65da36e3d5c27392b811579b3dd0f
raphaeluziel <raphaeluziel@hotmail.com>
2024-12-23 15:58:00 -0500

    reading errors

--- aaa/src/main.rs
+++ aaa/src/main.rs
@@ -1,55 +1,10 @@
+use std::fs::File;
 
 fn main() {
-    /*
-    struct Person { name: String, birth: i32, }
+    let greeting_file_result = File::open("hello.txt");
 
-    let mut composers = Vec::new();
-
-    composers.push(Person { name: "Palestrina".to_string(), birth: 1525 });
-    composers.push(Person { name: "Dowland".to_string(), birth: 1563 });
-    composers.push(Person { name: "Lully".to_string(), birth: 1632 });
-
-    for composer in &composers {
-        println!("{}, born {}", composer.name, composer.birth);
-    }
-    */
-
-    /*
-    for i in 0..3 {
-        println!("{} born in {}", composers[i].name, composers[i].birth);
-    }
-    println!("{}", composers[0].name);
-    
-
-    // Page 106
-    let x = 14;
-    let mut y = 78;
-
-    let rx = &x;
-    let sx = &x;
-    let tx = &mut y;
-
-    *tx = 9999;
-
-    println!("rx = {:p}", rx);
-    println!("sx = {:p}", sx);
-    println!("tx = {:p}", tx);
-
-    println!("y = {}", y);
-    */
-
-    // page 116 removing the * in *r < *s
-    fn smallest(v: &[i32]) -> &i32 {
-        let mut s = &v[0];
-        for r in &v[1..] {
-            if r < s { s = r; }
-        }
-        s
-    }
-    let s;
-    {
-        let parabola = [16, 9, 4, 2, 4, 9, 16];
-        s = smallest(&parabola);
-        println!("{}", *s);
-    }
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
 }
