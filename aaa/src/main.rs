127dba44ea5d9d31f567381f050fb0b4ffa3ba74
raphaeluziel <raphaeluziel@hotmail.com>
2024-12-24 16:31:56 -0500

    not much

--- aaa/src/main.rs
+++ aaa/src/main.rs
@@ -1,10 +1,18 @@
-use std::fs::File;
+use std::{fs::File, io::ErrorKind};
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
 
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {error:?}"),
+        Err(error) => {
+            match error.kind() {
+                ErrorKind::NotFound => match File::create("hello.txt") {
+                    Ok(fc) => fc,
+                    Err(e) => panic!("Problem creating the file {e:?}"),
+                }
+                other_error => panic!("Problem opening file {other_error:?}")
+            }
+        },
     };
 }
